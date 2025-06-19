dd01b816d4fccca1c90398b8d125bbdd2dc5fd49
Imper283 <115180828+Imper283@users.noreply.github.com>
2025-06-19 19:12:55 +0300

    Update tabConsole.py

--- components/tabConsole.py
+++ components/tabConsole.py
@@ -1,6 +1,7 @@
 import customtkinter as ctk
 import time
 import clipboard
+from tkinter import filedialog
 class Console(ctk.CTkFrame):
     def __init__(self, master):
         super().__init__(master)
@@ -16,13 +17,13 @@ class Console(ctk.CTkFrame):
 
         self.consoleControls.grid(row=0, column=0, sticky="nsew")
 
-        self.consoleSaveBtn = ctk.CTkButton(self.consoleControls, corner_radius=10, text="Save")
+        self.consoleSaveBtn = ctk.CTkButton(self.consoleControls, corner_radius=10, text="Save", command=self.__saveToFile)
         self.consoleSaveBtn.grid(row=0, column=0, sticky="nsew")
 
         self.consoleClipboardBtn = ctk.CTkButton(self.consoleControls, corner_radius=10, text="Save to clipboard", command=self.__saveToClipboard)
         self.consoleClipboardBtn.grid(row=0, column=1, sticky="nsew")
 
-        self.consoleClearBtn = ctk.CTkButton(self.consoleControls, corner_radius=10, text="Clear console")
+        self.consoleClearBtn = ctk.CTkButton(self.consoleControls, corner_radius=10, text="Clear console", command=self.__clearLogs)
         self.consoleClearBtn.grid(row=0, column=2, sticky="nsew")
 
         self.consoleContainer = ctk.CTkScrollableFrame(self); self.consoleContainer.grid(row=1, column=0, sticky="nsew")
@@ -37,6 +38,18 @@ class Console(ctk.CTkFrame):
 
     def __saveToClipboard(self):
         clipboard.copy(self.consoleData)
+    
+    def __saveToFile(self):
+        f = filedialog.asksaveasfile(mode='w', defaultextension=".txt")
+        if f is None: # asksaveasfile return `None` if dialog closed with "cancel".
+            return
+        textToSave = str(self.consoleData) # starts from `1.0`, not `0.0`
+        f.write(textToSave)
+        f.close() # `()` was missing.
+        
+    def __clearLogs(self):
+        self.consoleData = f"[{self.__calcTime()}][LOG]: Console cleared\n"
+        self.console.configure(text=self.consoleData)
 
     def log(self, text):
         self.consoleData = f"{self.consoleData}[{self.__calcTime()}][LOG]: {text}\n"
